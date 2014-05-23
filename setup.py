03a01e94fb8b1d92237ea3519b6e0330aa0f9731
Jon Crall <crallj@rpi.edu>
2014-05-23 19:51:29 -0400

    wip

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from __future__ import absolute_import, division, print_function
 if __name__ == '__main__':
     from utool.util_setup import setuptools_setup
     setuptools_setup(
-        package_name='plottool',
+        name='plottool',
         version='1.0.0.dev1',
         description=('Plottool - tools matplotlib computer vision plots'),
         url='https://github.com/Erotemic/plottool',
