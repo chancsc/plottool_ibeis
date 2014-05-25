6c5092c4d7fd3c6c5adac48ab1d415552211832d
joncrall <crallj@rpi.edu>
2014-05-25 18:10:59 -0400

    wip

--- setup.py
+++ setup.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python
 from __future__ import absolute_import, division, print_function
 
+INSTALL_REQUIRES = [
+    'matplotlib >= 1.3.1',
+]
+
 if __name__ == '__main__':
     from utool.util_setup import setuptools_setup
     setuptools_setup(
         name='plottool',
-        version='1.0.0.dev1',
         description=('Plottool - tools matplotlib computer vision plots'),
         url='https://github.com/Erotemic/plottool',
         keywords='',
