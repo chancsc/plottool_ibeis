bed8973a00eb0aa024f1b9f6f41772af5e2d3816
joncrall <crallj@rpi.edu>
2014-07-25 16:17:16 -0400

    new setup

--- setup.py
+++ setup.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python2.7
 from __future__ import absolute_import, division, print_function
+from setuptools import setup
 
 INSTALL_REQUIRES = [
     'matplotlib >= 1.3.1',
 ]
 
+
 if __name__ == '__main__':
     from utool.util_setup import setuptools_setup
-    setuptools_setup(
+    kwargs = setuptools_setup(
         name='plottool',
         description=('Plottool - tools matplotlib computer vision plots'),
         url='https://github.com/Erotemic/plottool',
@@ -18,3 +20,4 @@ if __name__ == '__main__':
         author_email='erotemic@gmail.com',
         setup_fpath=__file__,
     )
+    setup(**kwargs)
