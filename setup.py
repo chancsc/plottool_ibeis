c567b7fa8c1e27e0c6655b915dda0bb68a2891ae
Jon Crall <crallj@rpi.edu>
2014-08-07 17:59:03 -0400

    mpl init, better prints, setup

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python2.7
 from __future__ import absolute_import, division, print_function
 from setuptools import setup
+from utool import util_setup
 
 INSTALL_REQUIRES = [
     'matplotlib >= 1.3.1',
@@ -8,9 +9,11 @@ INSTALL_REQUIRES = [
 
 
 if __name__ == '__main__':
-    from utool.util_setup import setuptools_setup
-    kwargs = setuptools_setup(
+    kwargs = util_setup.setuptools_setup(
         name='plottool',
+        version=util_setup.parse_package_for_version('plottool'),
+        licence=util_setup.read_license('LICENSE'),
+        long_description=util_setup.parse_readme('README.md'),
         description=('Plottool - tools matplotlib computer vision plots'),
         url='https://github.com/Erotemic/plottool',
         keywords='',
