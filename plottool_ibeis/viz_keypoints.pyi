72110dece5be0fa794f2fe93105dfdc6fa734ba2
joncrall <erotemic@gmail.com>
2023-01-28 17:51:01 -0500

    Add type stubs

---
+++ plottool_ibeis/viz_keypoints.pyi
@@ -0,0 +1,16 @@
+from numpy import ndarray
+from typing import Union
+from typing import Any
+from typing import Any
+
+
+def testdata_kpts():
+    ...
+
+
+def show_keypoints(chip: ndarray,
+                   kpts: ndarray,
+                   fnum: int = 0,
+                   pnum: Union[tuple, None, Any] = None,
+                   **kwargs) -> None:
+    ...
